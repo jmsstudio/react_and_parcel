8854b1ccca63870365c979157ed474f6f63033e5
Jefferson M Souza <jefferson.souza@synchro.com.br>
2018-03-07 22:39:16 -0300

    first commit - parcel is working!

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <div>Parcel is working</div>
+      <input type="text" />
+      <h1>Title</h1>
+    </div>
+  );
+}
+
+export default App;
